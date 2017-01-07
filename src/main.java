e45cca045b686352a075c3daae3352a21ac99084
An Bk <andi.beka89@gmail.com>
2017-01-07 11:46:01 +0200

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,15 @@
+/** 
+ * @author: CodeFvr
+ */
+import kryptolekso.*;
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        GamePlayContainer g = new GamePlayContainer();
+        g.passOwnObject(g);
+        
+    }
+    
+}
